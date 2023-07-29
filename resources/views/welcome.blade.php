974dc4366dda2346125e98066f44c380a54fca30
tresna <tresna312@gmail.com>
2023-07-29 22:18:53 +0600

    portfolio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Tresna Rani</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet">  <!-- Core theme CSS (includes Bootstrap)-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&family=Roboto:wght@100&display=swap" rel="stylesheet">
+<link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body id="page-top mb-5">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top " id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Tresna Rani</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" style="width:300px;height:300px;" src="assets/img/profile.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>		 
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+					<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#project">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Tresna
+                        <span class="text-warning">Rani</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Bogura,Bangladesh · (880) 1737336101 ·
+                        <a href="mailto:tresna312@gmail.com">tresna312@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">I am experienced in Laravel frameworks to provide a high secure  website. I am also make a website using PHP,HTML,CSS, JavaScript and MySQL.</p>
+                    <div class="social-icons">
+                        <a class="social-icon linkedin" style="background-color:white;color:aqua;" href="https://www.linkedin.com/in/tresna-rani-b20b11248/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon github" style="background-color:white;color:black;" href="https://github.com/TresnaRani"><i class="fab fa-github"></i></a>
+                        <!--<a class="social-icon" href="#!"><i class="fab fa-twitter"></i></a> -->
+                        <a class="social-icon facebook" style="background-color:blue;color:white;" href="https://www.facebook.com/profile.php?id=100083832080295"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+					<br>
+					<br>
+                    <div class="cv">
+                    <a href="assets/file/cv.pdf" class="btn btn-info" target="_blank"download>Download my CV</a>
+                </div>
+                    </div>
+				
+					  
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0" style="font-family: 'Roboto', sans-serif;">Full Stack Web Developer (Laravel)</h3>
+                            <div class="subheading mb-3"> <a class="social-icon2" href="#" style="text-decoration: none;" target="_blank">STDevs</a></div>
+                            <p>Best communication with team member.Follow MVC pattern.Good performance at OOD(Object Oriented Design)</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">October 2021 - Present</span></div>
+                    </div>
+                  
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0"style="font-family: 'Roboto', sans-serif;">University of Rajshahi</h3>
+                            <div class="subheading mb-3">Bachelor of Science</div>
+                            <div>Computer Science and Engineering</div>
+                            <p>GPA: 3.10</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">February 2018 - January 2023</span></div>
+                    </div>
+                  
+                </div>
+            </section>
+            <hr class="m-0" />
+			
+		
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <label for="" style="font-size:20px;"><b>Html</b></label>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 100%; background-color:green;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">100%</div>
+                    </div>
+                    <label for="" style="font-size:20px;"><b>CSS</b></label>
+                    
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 98%; background-color:green;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">98%</div>
+                    </div>
+                    <label for="" style="font-size:20px;"><b>JS</b></label>
+                    
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 90%; background-color:green;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">90%</div>
+                    </div>
+                    <label for="" style="font-size:20px;"><b>Laravel</b></label>
+                    
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%; background-color:green;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">80%</div>
+                    </div>
+</br>
+                    <div class="subheading mb-3">Workflow</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Mobile-First, Responsive Design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Browser Testing & Debugging
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Cross Functional Teams
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Agile Development & Scrum
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+			
+				<!--Project-->
+			
+			 <section class="resume-section" id="project">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Projects</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            
+							 
+							 
+							</div>
+						 <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <div class="card-group">
+							  <div class="card" style="margin-right:10px;">
+								<img class="card-img-top" src="assets/img/p-4.png" alt="Card image cap">
+								<div class="card-body">
+								  <h5 class="card-title">Library Management System</h5>
+								  <p class="card-text">This library management system is software that is designed to manage all the functions of a library. It helps librarian to maintain the database of new books and the books that are borrowed by members along with their due dates. This system completely automates all your library's activities.</p>
+								  <a href="http://seminar-library-cse-ru.infinityfreeapp.com/" class="btn btn-success" target="_blank">View Demo</a>
+								</div>
+							  </div>
+							  <div class="card" style="margin-right:10px;">
+								<img class="card-img-top" src="assets/img/p-2.png" alt="Card image cap">
+								<div class="card-body">
+								  <h5 class="card-title">Advance Coaching Management System</h5>
+								  <p class="card-text">Advance Coaching Management System is a full Educational management software. You will find everything you need to run your Institution digitally.</p>
+								  <br>
+                                  <br>
+                                  <br>
+                                  <a href="#" class="btn btn-success">View Demo</a>
+								</div>
+							  </div>
+                              <div class="card ">
+								<img class="card-img-top" src="assets/img/p-5.png" alt="Card image cap">
+								<div class="card-body">
+								  <h5 class="card-title">Smart Bidding Tour With AI</h5>
+								  <p class="card-text">Our system is an end to end complete software solution which will help the tourists to provide service manager,booking pricing and other inventory.</p>
+								  <br>
+                                  <br>
+                                  <br>
+                                  <a href="https://smart.bidding.tour.evaxbd.com/" class="btn btn-success">View Demo</a>
+								</div>
+							  </div>
+							 
+							</div>
+                        </div>
+                     
+                    </div>
+                  
+                </div>
+            </section>
+			
+			  <hr class="m-0" />
+			
+			
+			
+           
+            <!-- Awards-->
+            <section class="resume-section" id="awards">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Awards & Certifications</h2>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                         
+                            Android App Components-Coursera
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
+                            Introduction to Web Development - Coursera
+                        </li>
+                      
+                     
+                    </ul>
+                </div>
+            </section>
+
+            <!--Section: Contact v.2-->
+<section class="mb-4" id="contact" style="margin: 50px 50px 50px 50px;margin-bottom:100px!important">
+
+<!--Section heading-->
+<h2 class="h1-responsive font-weight-bold text-center my-4">Contact</h2>
+<!--Section description-->
+<p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact directly.I will come back to you within
+    a matter of hours to help you.</p>
+
+<div class="row">
+
+    <!--Grid column-->
+    <div class="col-md-9 mb-md-0 mb-5">
+        <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="name" name="name" class="form-control">
+                        <label for="name" class="">Your name</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-6">
+                    <div class="md-form mb-0">
+                        <input type="text" id="email" name="email" class="form-control">
+                        <label for="email" class="">Your email</label>
+                    </div>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="md-form mb-0">
+                        <input type="text" id="subject" name="subject" class="form-control">
+                        <label for="subject" class="">Subject</label>
+                    </div>
+                </div>
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-12">
+
+                    <div class="md-form">
+                        <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
+                        <label for="message">Your message</label>
+                    </div>
+
+                </div>
+            </div>
+            <!--Grid row-->
+
+        </form>
+
+        <div class="text-center text-md-left">
+            <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+        </div>
+        <div class="status"></div>
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-md-3 text-center">
+        <ul class="list-unstyled mb-0">
+            <li><i class="fas fa-map-marker-alt fa-2x"></i>
+                <p>Bogura, Bangladesh</p>
+            </li>
+
+            <li><i class="fas fa-phone mt-4 fa-2x"></i>
+                <p>+ 01799993333</p>
+            </li>
+
+            <li><i class="fas fa-envelope mt-4 fa-2x"></i>
+                <p>tr@gmail.com</p>
+            </li>
+        </ul>
+    </div>
+    <!--Grid column-->
+
+</div>
+
+</section>
+<!--Section: Contact v.2-->
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
